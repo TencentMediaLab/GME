c12aa45bb9d38b27ee88c998d84255702162d210
蔡书翔 <35095055+shusencai@users.noreply.github.com>
2021-06-17 21:42:48 +0800

    Update Source.java

--- GME Developer Manual/signature/authentication_java/Source.java
+++ GME Developer Manual/signature/authentication_java/Source.java
@@ -2,10 +2,10 @@ import java.util.Base64;
 
 public class Source {
     public static void main(String [] args) {
-        int sdkAppID = 1400089356;
+        int sdkAppID = AppID To Replace Here ;
         byte[] strRoomID = "2018060920180609".getBytes();
         byte[] strOpenID = "95549554955495549554".getBytes();
-        byte[] strkey = "ae8c7e0428e341a2".getBytes();
+        byte[] strkey = "Key To Replace Here ".getBytes();
         byte[] ret = GMEAuthbuffer.QAVSDK_AuthBuffer_GenAuthBuffer(sdkAppID, strRoomID, strOpenID, strkey);
 
         String helloHex = Base64.getEncoder().encodeToString(ret);
