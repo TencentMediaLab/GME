1c3ec7f13352f7e6d957f420aa681426e2e4b0df
蔡书翔 <35095055+shusencai@users.noreply.github.com>
2021-06-17 21:43:21 +0800

    Update Source.java

--- GME Developer Manual/signature/authentication_java/Source.java
+++ GME Developer Manual/signature/authentication_java/Source.java
@@ -2,10 +2,10 @@ import java.util.Base64;
 
 public class Source {
     public static void main(String [] args) {
-        int sdkAppID = AppID To Replace Here ;
-        byte[] strRoomID = "2018060920180609".getBytes();
-        byte[] strOpenID = "95549554955495549554".getBytes();
-        byte[] strkey = "Key To Replace Here ".getBytes();
+        int sdkAppID = 14000xxxxx;                                  // 为云控制台获取的AppID
+        byte[] strkey = "ae8c7e0428xxxxx".getBytes();               // 为云控制台获取的Key， 与AppID成对
+        byte[] strRoomID = "2018060920180609".getBytes();           // 分配的房间号
+        byte[] strOpenID = "95549554955495549554".getBytes();       // 此用户的OpenID
         byte[] ret = GMEAuthbuffer.QAVSDK_AuthBuffer_GenAuthBuffer(sdkAppID, strRoomID, strOpenID, strkey);
 
         String helloHex = Base64.getEncoder().encodeToString(ret);
